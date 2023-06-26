53bd8cdbfdbb7c9cfb8739d650aae7440b8add77
Devin <70994866+RetroGamer02@users.noreply.github.com>
2023-06-26 17:36:05 -0400

    Small fixes

--- src/ck_config.h
+++ src/ck_config.h
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // (NOTE: You'll need to enable this on double-buffered backends for accuracy
 // at the moment, as VL_SyncPages() isn't implemented. The performance hit on
 // DOS isn't really worth it, though.)
-//#define ALWAYS_REDRAW
+#define ALWAYS_REDRAW
 
 // =================================
 // Options for DEBUG builds only.
